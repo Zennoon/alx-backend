f8a74dd6f9e8625e8be7ab9377b3fdd240405f28
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-17 18:37:16 +0300

    Fix pycodestyle issues

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -95,4 +95,3 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
-
