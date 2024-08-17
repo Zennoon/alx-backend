4d71f0c87c3146ac0d23beadb7f370f2a8b21188
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-17 17:39:21 +0300

    Try importing index_range instead of rewriting it

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 """
-Contains class with methods to create simple pagination from csv data
+Contains:
+    Classes
+    =======
+    Server - Mock server that hosts a dataset and is able to
+    service requests for that dataset with pagination and a method
+    that offers hypermedia pagination
+
+    Functions
+    =========
+    index_range - Helper function that receives page number and
+    page size and returns the appropriate range if indexes to return
+    from the dataset
 """
 import csv
-from typing import List
-index_range = __import__('0-simple_helper_function').index_range
+import math
+from typing import List, Mapping, Tuple
 
+index_range = __import__('0-simple_helper_function').index_range
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -16,10 +28,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Reads from csv file and returns the dataset.
-        Returns:
-            List[List]: The dataset.
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,26 +38,22 @@ class Server:
 
         return self.__dataset
 
-    @staticmethod
-    def assert_positive_integer_type(value: int) -> None:
-        """
-        Asserts that the value is a positive integer.
-        Args:
-            value (int): The value to be asserted.
-        """
-        assert type(value) is int and value > 0
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Returns a page of the dataset.
+        Receives a page number and the page size and returns the appropriate
+        page from the dataset (correct list of rows)
+
         Args:
-            page (int): The page number.
-            page_size (int): The page size.
+            page (int): The page number to retrieve
+            page_size (int): THe number of records / rows of the dataset
+            to be returned at a time
+
         Returns:
-            List[List]: The page of the dataset.
+            (list): The appropriate rows of the dataset corresponding to given
+            pagination parameters
         """
-        self.assert_positive_integer_type(page)
-        self.assert_positive_integer_type(page_size)
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         dataset = self.dataset()
         start, end = index_range(page, page_size)
         try:
@@ -57,23 +62,15 @@ class Server:
             data = []
         return data
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """
-        Returns a page of the dataset.
-        Args:
-            page (int): The page number.
-            page_size (int): The page size.
-        Returns:
-            List[List]: The page of the dataset.
-        """
-        total_pages = len(self.dataset()) // page_size + 1
-        data = self.get_page(page, page_size)
-        info = {
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Mapping:
+        dataset = self.dataset()
+        rows = self.get_page(page=page, page_size=page_size)
+        max_page_num = len(dataset) // page_size + 1
+        return {
+            "page_size": page_size if page_size <= len(rows) else len(rows),
             "page": page,
-            "page_size": page_size if page_size <= len(data) else len(data),
-            "total_pages": total_pages,
-            "data": data,
-            "prev_page": page - 1 if page > 1 else None,
-            "next_page": page + 1 if page + 1 <= total_pages else None
+            "data": rows,
+            "next_page": page + 1 if page + 1 <= max_page_num else None,
+            "prev_page": page - 1 if page - 1 > 0 else None,
+            "total_pages": max_page_num
         }
-        return info
