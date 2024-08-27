184bbdd525c7f5ade03529e4ee2a710fa3491af2
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-27 20:42:57 +0300

    Modify the get_locale function to check the url for locale preferences

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Module-level
+    ============
+    app - A flask application
+    babel - A Babel instance to support multiple languages in our app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration for the flask application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """
+    Selects most appropriate locale from the supported
+    based on the request's Accept-Languages header value
+    """
+    url_locale = request.args.get("locale")
+    if url_locale and url_locale in app.config["LANGUAGES"]:
+        return url_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """Handles the root (/) route"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
